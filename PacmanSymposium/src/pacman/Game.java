5b5e35cc372e59e738e152e10963b06009207074
student 8 <student 8@10.8.33.151>
2017-05-11 13:37:18 -0400

    Updated Game class

--- PacmanSymposium/src/pacman/Game.java
+++ PacmanSymposium/src/pacman/Game.java
@@ -3,7 +3,9 @@ package pacman;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Scanner;
 
+import gui.components.Graphic;
 import gui.components.Visible;
 import gui.practice.Screen;
 
@@ -12,6 +14,8 @@ import gui.practice.Screen;
 public class Game extends Screen implements Runnable, KeyListener{
 	
 	private Player player;
+	private boolean gameStart = true;
+	private Graphic pic;
 	
 	public Game(int width, int height){
 		super(width, height);
@@ -20,37 +24,52 @@ public class Game extends Screen implements Runnable, KeyListener{
 	}
 
 	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
 	public void initObjects(ArrayList<Visible> viewObjects) {
-		//ask player for name
-		player = new Player();
+		System.out.println("Enter player name.");
+		Scanner s = new Scanner(System.in);
+		String name = s.nextLine();
+		player = new Player(name, 0, 0, 0);
+		
+		pic = new Graphic(10, 10, "resources/Pacman-OpenMouth.png");
+		viewObjects.add(pic);
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
-		//System.out.println("Key was pressed");
-		player.update();
+	public void keyPressed(KeyEvent e) {
+		if(gameStart == true){
+			if(e.getKeyCode() == KeyEvent.VK_UP){
+				player.update("UP");
+			}
+			if(e.getKeyCode() == KeyEvent.VK_DOWN){
+				player.update("DOWN");
+			}
+			if(e.getKeyCode() == KeyEvent.VK_LEFT){
+				player.update("LEFT");
+			}
+			if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+				player.update("RIGHT");
+			}
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent arg0) {
-		//System.out.println("Key was released");
-		
+		//System.out.println("Key was released");	
 	}
 
 	@Override
 	public void keyTyped(KeyEvent arg0) {
-		//System.out.println("Key was typed");
-		
+		//System.out.println("Key was typed");	
 	}
 	
 	public KeyListener getKeyListener() {
 		return this;
+	}
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
