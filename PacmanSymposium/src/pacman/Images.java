d2cab9758bb3dcbe42ce17b0d13c41fc28a32c99
Student 6 <Student 6@10.8.33.137>
2017-05-12 13:58:31 -0400

    enemy

--- PacmanSymposium/src/pacman/Images.java
+++ PacmanSymposium/src/pacman/Images.java
@@ -1,11 +1,17 @@
 package pacman;
 
+import java.util.ArrayList;
+
+import gui.components.Graphic;
+
 /**
  * @author Risa
  *
  */
 
 public class Images {
+	
+	private ArrayList<Graphic> graphicList = new ArrayList<Graphic>();
 	
 	private boolean wall;
 	private boolean food;
@@ -18,6 +24,8 @@ public class Images {
 	private int height;
 	
 	public Images(String imageSrc, boolean wall, boolean food, boolean powerUp, int x, int y, int width, int height){
+		graphicList.add(new Graphic(x, y, imageSrc));
+		
 		this.wall = wall;
 		this.imageSrc = imageSrc;
 		this.x = x;
