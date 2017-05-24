44d00ffedc74b26b32eb4d7d5ec455ef977f526d
student 8 <student 8@10.8.33.151>
2017-05-24 13:52:28 -0400

    Continuing Map

--- PacmanSymposium/src/pacman/Map.java
+++ PacmanSymposium/src/pacman/Map.java
@@ -8,27 +8,43 @@ import gui.components.Component;
 public class Map extends Component{
 	
 	private int[][] coordinates;
+	private int width;
+	private int height;
+	private int size;
 	
 	public Map(int x, int y, int w, int h) {
 		//width and height is divisible by 75
 		super(x, y, w, h);
+		width = w;
+		height = h;
 	}
 
 	@Override
 	public void update(Graphics2D g) {
+		size = 30;
 		g.setColor(Color.black);
 		for(int i = 0; i < 25; i++){
-			g.drawRect(30*i + 10, 30, 30, 30);
+			if(i != 13){
+				g.drawRect(30*i + 10, 30, size, size);
+			}		
 		}
 		for(int i = 1; i < 20; i++){
-			g.drawRect(10, 30*i + 30, 30, 30);
+			if(i != 10){
+				g.drawRect(10, 30*i + 30, size, size);
+			}		
 		}
 		for(int i = 0; i < 25; i++){
-			g.drawRect(30*i + 10, 630, 30, 30);
+			if(i != 13){
+				g.drawRect(30*i + 10, 630, size, size);
+			}		
 		}
 		for(int i = 1; i < 20; i++){
-			g.drawRect(730, 30*i + 30, 30, 30);
+			if(i != 10){
+				g.drawRect(730, 30*i + 30, size, size);
+			}	
 		}
+		
+		
 	}
 
 }
