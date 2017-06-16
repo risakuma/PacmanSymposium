56f254a4c5ee584081b7adee14aec38b77bf53c3
BigK <BigK@RayForComWin7090>
2017-06-15 20:02:09 -0400

    Almost done with Intro

--- PacmanSymposium/src/pacman/IntroScreen.java
+++ PacmanSymposium/src/pacman/IntroScreen.java
@@ -17,16 +17,22 @@ import gui.practice.Screen;
 public class IntroScreen extends Screen implements MouseMotionListener, MouseListener{
 	
 	private ClickableGraphic startBG;
+	private ClickableGraphic textBox;
+//	private ClickableGraphic textBox2;
+//	private ClickableGraphic textBox3;
+	private Graphic alice;
+	private Graphic rabbit;
+	private Graphic cheshire;
 	private Graphic storyBG1;
+	private Graphic storyBG2;
+	private Graphic storyBG3;
 	private TextLabel text;
 	private TextLabel text2;
 	private TextLabel story;
-	private Button textBox;
 	private Button gameButton;
 
 	public IntroScreen(int width, int height) {
 		super(width, height);
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
@@ -55,28 +61,183 @@ public class IntroScreen extends Screen implements MouseMotionListener, MouseLis
 				storyBG1 = new Graphic(20, 40, 1200, 730, "resource/BlackScreen.jpeg");
 				viewObjects.add(storyBG1);
 				
-				textBox = new Button(40, 550, 1160, 150, "...Alice found herself helplessly plummeting into darkness. For how long"
-						+ " she could not tell, but finally a ray of light beneath her began" 
-						+ "\n" + " to glow increasingly brighter.", new Color(224, 224, 224), new Action(){
+				textBox = new ClickableGraphic(40, 550, 1140, 150, "resource/textbox.png");
+				viewObjects.add(textBox);
+				
+				story = new TextLabel(40, 450, 1160, 150, "...Alice found herself helplessly plummeting into darkness until"
+						+ " a ray of light beneath her began to glow...(Next).");
+				story.setColor(Color.black);
+				viewObjects.add(story);
+				
+				textBox.setAction(new Action(){
 					public void act(){
+						viewObjects.remove(storyBG1);
 						
+						storyBG2 = new Graphic(20, 40, 1200, 730, "resource/Woodland.jpg");
+						viewObjects.add(storyBG2);
+						
+						story.setText("...Then a bright flash of green illuminated the surrounding and birds could be heard"
+								+ " chirping melodious tunes... (Next)");
+						viewObjects.add(story);
 					}
 				});
+			}
+		
+		});
+		startBG.setAction(new Action(){
+			public void act(){
+				storyBG2 = new Graphic(20, 40, 1200, 730, "resource/Woodland.jpg");
+				viewObjects.add(storyBG2);
+				
+				textBox = new ClickableGraphic(40, 550, 1140, 150, "resource/textbox.png");
 				viewObjects.add(textBox);
+				
+				story = new TextLabel(40, 450, 1160, 150, "...Alice slowly opened her eyes and found herself in a forest... (Next)");
+				story.setColor(Color.black);
+				viewObjects.add(story);		
+				
+				textBox.setAction(new Action(){
+					public void act(){		
+						alice = new Graphic(1000, 210, 245, 300, "resource/AliceA.png");
+						viewObjects.add(alice);
+								
+						story.setText("Alice: H-huh? Where am I? How did I become so tiny? (Next)");
+						viewObjects.add(story);
+					}
+				});
 			}
 		});
-		
-
-		
-		
-		
-		
-//		gameButton = new Button(40, 200, 150, 40, "Start Game", Color.blue, new Action(){
-//			public void act(){
-//				Intro.intro.setScreen(Intro.gameScreen);
-//			}
-//		});
-//		viewObjects.add(gameButton);
+		startBG.setAction(new Action(){
+			public void act(){
+				storyBG2 = new Graphic(20, 40, 1200, 730, "resource/Woodland.jpg");
+				viewObjects.add(storyBG2);
+				
+				cheshire = new Graphic(65, 300, 180, 150, "resource/Cheshire2.jpg");
+				viewObjects.add(cheshire);
+				
+				alice = new Graphic(1000, 230, 220, 275, "resource/AliceA.png");
+				viewObjects.add(alice);	
+				
+				textBox = new ClickableGraphic(40, 550, 1140, 150, "resource/textbox.png");
+				viewObjects.add(textBox);
+				
+				story = new TextLabel(40, 450, 1160, 150, "Cheshire: Are you lost? (Next)");
+				story.setColor(Color.black);
+				viewObjects.add(story);									
+				
+				textBox.setAction(new Action(){
+					public void act(){
+						viewObjects.remove(alice);
+						
+						alice = new Graphic(710, 285, 245, 300, "resource/AliceA.png");
+						viewObjects.add(alice);
+								
+						story.setText("Alice: What? No, I'm not. I mean...wait you can talk? (Next)");
+						viewObjects.add(story);
+					}
+				});
+			}
+		});
+		startBG.setAction(new Action(){
+			public void act(){
+				storyBG2 = new Graphic(20, 40, 1200, 730, "resource/Woodland.jpg");
+				viewObjects.add(storyBG2);
+				
+				cheshire = new Graphic(325, 350, 150, 120, "resource/Cheshire2.jpg");
+				viewObjects.add(cheshire);
+				
+				alice = new Graphic(600, 300, 245, 300, "resource/AliceA.png");
+				viewObjects.add(alice);	
+				
+				textBox = new ClickableGraphic(40, 550, 1140, 150, "resource/textbox.png");
+				viewObjects.add(textBox);
+				
+				story = new TextLabel(40, 450, 1160, 150, "Cheshire: Well yes, but if you think that's more important than escaping this forest"
+						+ " I guess we can go talking forever... Or you ");
+				TextLabel extra = new TextLabel(40, 500, 1160, 150, "can follow the White Rabbit over there. (Next)");
+				story.setColor(Color.black);
+				extra.setColor(Color.black);
+				viewObjects.add(story);			
+				viewObjects.add(extra);
+				
+				textBox.setAction(new Action(){
+					public void act(){	
+						viewObjects.remove(extra);
+						
+						rabbit = new Graphic(575, 275, 50, 50, "resource/Rabbit.jpg");
+						viewObjects.add(rabbit);
+								
+						story.setText("White Rabbit: I'm late I'm late I'm late! (Next)");
+						viewObjects.add(story);
+					}
+				});
+			}
+		});
+		startBG.setAction(new Action(){
+			public void act(){
+				viewObjects.remove(storyBG2);
+				viewObjects.remove(rabbit);
+				viewObjects.remove(cheshire);
+				viewObjects.remove(alice);
+				
+				storyBG3 = new Graphic(20, 40, 1200, 730, "resource/Castle.jpg");
+				viewObjects.add(storyBG3);
+				
+				alice = new Graphic(1000, 300, 220, 275, "resource/AliceA.png");
+				viewObjects.add(alice);	
+				
+				textBox = new ClickableGraphic(40, 550, 1140, 150, "resource/textbox.png");
+				viewObjects.add(textBox);
+				
+				story = new TextLabel(40, 450, 1160, 150, "Following the words of the Cheshire Cat, Alice followed the White Rabbit into a"
+						+ " grand castle. (Next)");
+				story.setColor(Color.black);
+				viewObjects.add(story);									
+				
+				textBox.setAction(new Action(){
+					public void act(){	
+						viewObjects.remove(alice);
+						
+						story.setText("Queen: Who dares to enter without my permission? Off with her head! (Next)");
+						viewObjects.add(story);
+					}
+				});
+			}
+		});
+		startBG.setAction(new Action(){
+			public void act(){	
+				storyBG3 = new Graphic(20, 40, 1200, 730, "resource/Castle.jpg");
+				viewObjects.add(storyBG3);	
+				
+				textBox = new ClickableGraphic(40, 550, 1140, 150, "resource/textbox.png");
+				viewObjects.add(textBox);
+				
+				story = new TextLabel(40, 450, 1160, 150, "The guards took Alice each by her hands and marched towards the dungeon."
+						+ " The Cheshire Cat, who had been watching afar,");
+				TextLabel extra = new TextLabel(40, 500, 1160, 150, " comes forward and proposes a deal with the queen. (Next)");
+				story.setColor(Color.black);
+				extra.setColor(Color.black);
+				viewObjects.add(story);		
+				viewObjects.add(extra);
+				
+				textBox.setAction(new Action(){
+					public void act(){			
+						viewObjects.remove(extra);
+						
+						story.setText("Cheshire: If Alice can bring you your favorite strawberries from the Haunted Forest, will you"
+								+ " let her leave?");
+						viewObjects.add(story);
+						
+						gameButton = new Button(1000, 650, 150, 40, "Start Game", new Color(204, 255, 255), new Action(){
+							public void act(){
+								Intro.intro.setScreen(Intro.gameScreen);
+							}
+						});
+						viewObjects.add(gameButton);
+					}
+				});
+			}
+		});
 	}
 
 	@Override
@@ -87,8 +248,8 @@ public class IntroScreen extends Screen implements MouseMotionListener, MouseLis
 		if(textBox.isHovered(e.getX(), e.getY())){
 			textBox.act();
 		}
-//		if(gameButton.isHovered(e.getX(), e.getY()))
-//			gameButton.act();
+		if(gameButton.isHovered(e.getX(), e.getY()))
+			gameButton.act();
 	}
 	
 	public MouseListener getMouseListener(){
